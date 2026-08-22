a824df93e065894f32cb10286ea3c082da355ffd
TearG <faria.info@gmail.com>
2026-08-22 05:04:31 -0300

    chore: migrate React 16 to React 18, Typescript 4

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,9 @@
 import * as React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 
 import App from "./App";
 
 const rootElement = document.getElementById("root");
-render(<App />, rootElement);
+if (!rootElement) throw new Error('Root element not found');
+const root = createRoot(rootElement);
+root.render(<App />);
